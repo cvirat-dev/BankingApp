b1a20d6e271430c58abe91b45d0f46de4173735b
ClementVirat <clement.virat2@gmail.com>
2026-06-20 09:07:25 +0200

    feat: Implement notification types for transactions and create related entities

---
+++ benachrichtigung-service/src/main/java/com/demo/benachrichtigung_service/benachrichtigung/AktionTyp.java
@@ -0,0 +1,7 @@
+package com.demo.benachrichtigung_service.benachrichtigung;
+
+public enum AktionTyp {
+    ERSTELLEN,
+    AKTUALISIEREN,
+    LOESCHEN
+}
---
+++ benachrichtigung-service/src/main/java/com/demo/benachrichtigung_service/benachrichtigung/KontoBenachrichtigung.java
@@ -0,0 +1,17 @@
+package com.demo.benachrichtigung_service.benachrichtigung;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("KONTOVERWALTUNG")
+@Getter @Setter @NoArgsConstructor
+public class KontoBenachrichtigung extends Benachrichtigung {
+    private Long kontoId;
+    private String iban;
+    private String inhaber;
+    private AktionTyp aktion;
+}
