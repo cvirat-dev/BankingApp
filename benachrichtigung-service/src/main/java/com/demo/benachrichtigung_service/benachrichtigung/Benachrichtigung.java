02a26b09ae986a749671b8a0cdbb45e10d34867f
ClementVirat <clement.virat2@gmail.com>
2026-09-11 13:22:04 +0200

    feat: Update Benachrichtigung and KontoBenachrichtigung to use EnumType.STRING for notification types and actions

--- benachrichtigung-service/src/main/java/com/demo/benachrichtigung_service/benachrichtigung/Benachrichtigung.java
+++ benachrichtigung-service/src/main/java/com/demo/benachrichtigung_service/benachrichtigung/Benachrichtigung.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,6 +24,7 @@ public class Benachrichtigung {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private BenachrichtigungTyp typ;
     private String nachricht;
     private LocalDateTime timestamp;
